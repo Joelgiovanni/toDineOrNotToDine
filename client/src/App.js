5f03ef536242e130ad3705707d7117634e0661a2
Joel Godoy <joelgiovanni4@gmail.com>
2019-06-01 14:52:20 -0600

    UI init. Parallax test

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Main from './components/layout/Main';
+import Banner from './components/layout/Banner';
 
 function App() {
   return (
     <div className='App'>
-      <Main />
+      <Banner />
     </div>
   );
 }
---
+++ client/src/components/layout/Banner.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Banner.css';
+
+class Banner extends Component {
+  render() {
+    return (
+      <div>
+        <div className='parallax '>
+          <h1>YEA</h1>
+        </div>
+        <div className='test'>test</div>
+      </div>
+    );
+  }
+}
+
+export default Banner;
